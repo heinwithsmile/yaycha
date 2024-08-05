77e6148a361df2ac2b63efa7275cb350e6ddd3fa
heinwithsmile <heinhtetaung7715@gmail.com>
2024-08-06 00:03:24 +0630

    secod

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import Item from "./Item";
-
+import { useState } from "react";
 import List from "./List";
 import Item from "./Item";
-import { useState } from "react";
 
 function App(){
   const [data, setData] = useState([
     {id: 1, content: "Hello, World!", name: "John"},
     {id: 2, content: "React is fun.", name: "Bob"},
     {id: 3, content: "Yay, interesting.", name: "Doe"}
-  ])
+  ]);
+
+  const remove = id => {
+    setData(data.filter(item => item.id !== id));
+  };
 
   return (
     <div style={{
@@ -18,9 +20,12 @@ function App(){
     }}>
       <h1>Yaycha</h1>
       <List>
-        {data.map(item => {
-          return <Item content={item.content} name={item.name}/>
-        })}
+        {data.map(item => (
+          <Item 
+          key={item.id}
+          item={item} 
+          remove={remove}/>
+        ))}
       </List>
     </div>
   )
